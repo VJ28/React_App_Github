e6846eaba4b103556cf0a90e8b7a980eb1d2f9d9
VJ28 <vijaymourya28@gmail.com>
2019-09-08 23:33:21 +0530

    build

---
+++ build/precache-manifest.5a8e6ae25bb1075f7a9f113da17f2194.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ab2a9a39c3450364a0f68f0c08127b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10b064f8bb291a25a316",
+    "url": "/static/css/main.b1d5974c.chunk.css"
+  },
+  {
+    "revision": "1d98f2325bf426708ded",
+    "url": "/static/js/2.95e04b00.chunk.js"
+  },
+  {
+    "revision": "10b064f8bb291a25a316",
+    "url": "/static/js/main.5cbef403.chunk.js"
+  },
+  {
+    "revision": "a5f755efe99bf8f12704",
+    "url": "/static/js/runtime~main.6d6e1a4e.js"
+  }
+]);
